a078b954db6955a2fadf9fef7c50836647a3858d
Nigel Fitzgerald <johnscottnigel@gmail.com>
2023-04-17 16:07:26 +0200

    rails new and added play page

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -5,3 +5,19 @@ export default class extends Controller {
     this.element.textContent = "Hello World!"
   }
 }
+
+
+
+const options = document.querySelectorAll("#table td");
+
+
+
+options.forEach((box) => {
+  box.addEventListener("click", (event) => {
+    console.log(event);
+    box.innerHTML = "0";
+    box.classList.add("clicked");
+    box.disabled = true
+    box.nextElementSibling.innerHTML = "X"
+  })
+});
