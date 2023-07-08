efc55ba2bc0bfe4899d60517ce61c441ef1243e7
Nigel Fitzgerald <johnscottnigel@gmail.com>
2023-07-08 13:02:18 +0200

    added part of the logic that checks if player won

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -15,10 +15,17 @@ const options = document.querySelectorAll("#table td");
 options.forEach((box) => {
   box.addEventListener("click", (event) => {
     console.log(event);
+    console.log(box.className);
     if (box.classList.contains("empty")) {
       box.innerHTML = "0";
       box.classList.add("user_clicked");
       box.classList.remove("empty");
+    }
+
+    checkIfPlayerWins();
+    if (checkIfPlayerWins()) {
+      const header = document.querySelector('.header');
+      header.insertAdjacentHTML('beforeend', '<p>You win😎</p>');
     }
 
     const optionsLeft = document.querySelectorAll(".empty")
@@ -33,17 +40,10 @@ options.forEach((box) => {
 });
 
 
-
-// const months = ["January", "February", "March", "April", "May", "June", "July"];
-
-// const random = Math.floor(Math.random() * months.length);
-// console.log(random, months[random]);
-
-
-// if (document.getElementById("1").innerHTML === document.getElementById("2").innerHTML === document.getElementById("3").innerHTML) {
-//   if (document.getElementById("1").innerHTML === "X") {
-//     console.log("Computer wins")
-//   } else {
-//     console.log("You win")
-//   }
-// }
+const checkIfPlayerWins = () => {
+  const boxes = document.querySelectorAll('td');
+  if (boxes[0].classList.contains('user_clicked') && boxes[1].classList.contains('user_clicked') && boxes[2].classList.contains('user_clicked')) {
+    console.log("User wins");
+    return true;
+  }
+};
